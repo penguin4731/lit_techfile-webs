0be6b9b980192d492175586ec5cfac60ed3644f4
penguin4731 <penguin4731@gmail.com>
2020-04-30 14:54:04 +0000

    first commit

---
+++ app.rb
@@ -0,0 +1,56 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require 'open-uri'
+require 'tempfile'
+require 'sinatra/json'
+require './models/contribution.rb'
+
+before do
+  Dotenv.load
+  Cloudinary.config do |config|
+    config.cloud_name = ENV['CLOUD_NAME']
+    config.api_key= ENV['CLOUDINARY_API_KEY']
+    config.api_secret= ENV['CLOUDINARY_API_SECREAT']
+  end
+end
+
+get '/' do
+  @contents = Contribution.all.order('id desc')
+  erb :index
+end
+
+get '/edit/:id' do
+  @content = Contribution.find(params[:id])
+  erb :edit
+end
+
+post '/new' do
+  img_url = ''
+  if params[:file] != "" then
+    img = params[:file]
+    tempfile = img[:tempfile]
+    upload = Cloudinary::Uploader.upload(tempfile.path)
+    img_url = upload['url']
+  end
+  Contribution.create({
+    name: params[:user_name],
+    body: params[:body],
+    img: img_url
+  })
+  redirect '/'
+end
+
+post '/delete/:id' do
+  Contribution.find(params[:id]).destroy
+  redirect '/'
+end
+
+post '/renew/:id' do
+  content = Contribution.find(params[:id])
+  content.update({
+    name: params[:user_name],
+    body: params[:body]
+  })
+  redirect '/'
+end
