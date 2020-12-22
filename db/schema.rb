c02bdea110c9b7e61cca76d8321e426a36df4237
penguin4731 <penguin4731@gmail.com>
2020-12-22 03:01:25 +0000

    [add] category 5

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_22_021021) do
+ActiveRecord::Schema.define(version: 2020_12_22_025949) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "title"
