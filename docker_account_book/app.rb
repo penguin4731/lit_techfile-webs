15f1ff16a4da3e9efc428f9e46fcf494b640c7f4
Yo Higashida <y.higashida0226+app@gmail.com>
2022-01-06 18:51:15 +0900

    add docker_account_book

---
+++ docker_account_book/app.rb
@@ -0,0 +1,52 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+
+require './models/item.rb'
+
+get '/' do
+  @items = Item.all
+  @total = Item.sum(:price)
+  @categories = Category.all
+  erb :index
+end
+
+get '/category/:id' do
+  @categories = Category.all
+  @category = Category.find(params[:id])
+  @category_name = @category.name
+  @items = @category.items
+  erb :index
+end
+
+get '/edit/:id' do
+  @item = Item.find(params[:id])
+  @categories = Category.all
+  erb :edit
+end
+
+post '/create' do
+  Item.create({
+    title: params[:title],
+    price: params[:price],
+    category_id: params[:category]
+  })
+
+  redirect '/'
+end
+
+post '/delete/:id' do
+  Item.find(params[:id]).destroy
+  redirect '/'
+end
+
+post '/renew/:id' do
+  item = Item.find(params[:id])
+  item.update({
+    title: params[:title],
+    price: params[:price],
+    category_id: params[:category]
+  })
+
+  redirect '/'
+end
